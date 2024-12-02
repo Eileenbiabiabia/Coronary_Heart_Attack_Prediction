a6fd05edee2a65ed6de9a43d5c1d46f20c787efe
Eileenbiabiabia <eileenchuu@gmail.com>
2024-12-02 17:45:51 -0600

    update app

--- app.py
+++ app.py
@@ -8,8 +8,8 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-# with open('model.pkl', 'rb') as file:
-#     model = pickle.load(file)
+with open('model.pkl', 'rb') as file:
+    model = pickle.load(file)
 
 @app.route("/predict", methods=["POST"])
 def predict():
